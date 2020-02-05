2079c03c771c9d0cdd812e0a0a0b6594fe7d5772
hdorgeval <henri.dorgeval@outlook.fr>
2020-02-05 23:48:24 +0100

    chore(coverage): set coverage threshold to 90%

--- jest.config.js
+++ jest.config.js
@@ -35,10 +35,10 @@ module.exports = {
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
     },
   },
 
