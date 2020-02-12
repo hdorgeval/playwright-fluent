56402cf100977034723b03425238f5c8cd650d8b
hdorgeval <henri.dorgeval@outlook.fr>
2020-02-12 23:19:39 +0100

    feat(dom-actions): be able to check if a selector exists

---
+++ src/actions/dom-actions/exists/exists.ts
@@ -0,0 +1,23 @@
+import { Page } from 'playwright';
+
+export async function exists(selector: string, page: Page | undefined): Promise<boolean> {
+  if (!page) {
+    throw new Error(`Cannot check that '${selector}' exists because no browser has been launched`);
+  }
+
+  try {
+    const result = await page.$(selector);
+
+    if (result === null) {
+      return false;
+    }
+    return true;
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.warn(
+      `An internal error has occured in Playwright API while checking if selector '${selector}'  exists`,
+      error,
+    );
+    return false;
+  }
+}
