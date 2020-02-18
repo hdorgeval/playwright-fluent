a078f8d4ad4ff7c8cba5d81a697a91e74beb8107
hdorgeval <henri.dorgeval@outlook.fr>
2020-02-18 22:35:44 +0100

    test(selector): add test to showcase the Selector API

---
+++ src/selector/samples/ag-grid.chromium.test.ts
@@ -0,0 +1,42 @@
+import { PlaywrightController } from '../../controller';
+
+describe('Selector API - AG Grid samples', (): void => {
+  let pwc: PlaywrightController;
+  beforeEach((): void => {
+    jest.setTimeout(60000);
+    pwc = new PlaywrightController();
+  });
+  afterEach(
+    async (): Promise<void> => {
+      await pwc.close();
+    },
+  );
+
+  test.skip('should check', async (): Promise<void> => {
+    // Given I open The AG Grid demo site
+    const url = 'https://www.ag-grid.com/example.php';
+    await pwc
+      .withBrowser('chromium')
+      .withCursor()
+      .withOptions({ headless: false })
+      .navigateTo(url);
+
+    // When I select Olivia Brenan's name
+    const agGridContainer = pwc.selector('div.ag-body-viewport');
+    const checkbox = agGridContainer
+      .find('div[role="row"]')
+      .withText('Olivia Brennan')
+      .nth(1) // take the first row that contains 'Olivia Brennan'
+      .find('div[col-id="name"]') // take the cell in column name
+      .find('span.ag-selection-checkbox'); // take the checkbox in that cell
+
+    const handle = await checkbox.getFirstHandleOrNull(); // get the Playwright's element handle
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    await handle!.hover();
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    await handle!.click();
+
+    // Then
+    expect(handle).not.toBeNull();
+  });
+});
