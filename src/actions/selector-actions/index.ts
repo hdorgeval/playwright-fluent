f972111dbd46e1846677f4a9aaf7be530723d9d0
hdorgeval <henri.dorgeval@outlook.fr>
2020-02-24 13:21:40 +0100

    feat(selector-actions): be able to check if a selector is enabled

--- src/actions/selector-actions/index.ts
+++ src/actions/selector-actions/index.ts
@@ -1,3 +1,4 @@
 export * from './has-selector-object-focus';
 export * from './hover-on-selector-object';
+export * from './is-selector-object-enabled';
 export * from './is-selector-object-visible';
---
+++ src/actions/selector-actions/is-selector-object-enabled/is-selector-object-enabled.ts
@@ -0,0 +1,26 @@
+import { waitUntil, report, WaitUntilOptions } from '../../../utils';
+import { SelectorController } from '../../../selector';
+import { isHandleEnabled } from '../../handle-actions';
+import { Page } from 'playwright';
+
+export async function isSelectorObjectEnabled(
+  selector: SelectorController,
+  page: Page | undefined,
+  options: WaitUntilOptions,
+): Promise<boolean> {
+  if (!page) {
+    throw new Error(
+      `Cannot get enabled status of '${selector.toString()}' because no browser has been launched`,
+    );
+  }
+
+  report('waiting for the selector to appear in DOM ...', options.verbose);
+  await waitUntil(
+    () => selector.exists(),
+    `Cannot get enabled status of '${selector.toString()}' because this selector was not found in DOM`,
+    options,
+  );
+
+  const handle = await selector.getHandle();
+  return await isHandleEnabled(handle, { verbose: options.verbose });
+}
