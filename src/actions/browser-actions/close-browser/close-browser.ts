e5e43e8d4d3b61337f0a073cba5038dcb3c4ee19
hdorgeval <henri.dorgeval@outlook.fr>
2020-02-27 13:33:47 +0100

    fix(browser-actions): close browserContexts first before closing the browser

--- src/actions/browser-actions/close-browser/close-browser.ts
+++ src/actions/browser-actions/close-browser/close-browser.ts
@@ -4,5 +4,23 @@ export async function closeBrowser(browser: Browser | undefined): Promise<void> 
   if (browser === undefined) {
     return;
   }
-  await browser.close();
+
+  const contexts = browser.contexts();
+  if (Array.isArray(contexts) && contexts.length > 0) {
+    contexts.forEach((context) => {
+      try {
+        context.close();
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.warn('Error while closing the browser context', error);
+      }
+    });
+  }
+
+  try {
+    await browser.close();
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.warn('Error while closing the browser', error);
+  }
 }
