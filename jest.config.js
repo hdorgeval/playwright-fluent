2333384d8d2cd4eaa17d2feaf0f0e6f1cb208427
hdorgeval <henri.dorgeval@outlook.fr>
2020-02-27 12:26:21 +0100

    chore(coverage): lower branches threshold

--- jest.config.js
+++ jest.config.js
@@ -46,7 +46,7 @@ module.exports = {
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
-      branches: 90,
+      branches: 75,
       functions: 90,
       lines: 90,
       statements: 90,
