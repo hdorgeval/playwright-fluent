b30409f9054fa780998de2adba1a6230c3c03ad3
hdorgeval <henri.dorgeval@outlook.fr>
2020-02-28 20:01:24 +0100

    feat(selector-actions): be able to check if a selector is not visible

--- src/actions/selector-actions/index.ts
+++ src/actions/selector-actions/index.ts
@@ -2,4 +2,5 @@ export * from './has-selector-object-focus';
 export * from './hover-on-selector-object';
 export * from './is-selector-object-disabled';
 export * from './is-selector-object-enabled';
+export * from './is-selector-object-not-visible';
 export * from './is-selector-object-visible';
---
+++ src/actions/selector-actions/is-selector-object-not-visible/is-selector-object-not-visible.ts
@@ -0,0 +1,25 @@
+import { waitUntil, report, WaitUntilOptions } from '../../../utils';
+import { SelectorController } from '../../../selector';
+import { isHandleVisible, isHandleNotVisible } from '../../handle-actions';
+import { Page } from 'playwright';
+
+export async function isSelectorObjectNotVisible(
+  selector: SelectorController,
+  page: Page | undefined,
+  options: WaitUntilOptions,
+): Promise<boolean> {
+  if (!page) {
+    throw new Error(
+      `Cannot get visibility status of '${selector.toString()}' because no browser has been launched`,
+    );
+  }
+
+  const isVisible = await isHandleVisible(await selector.getHandle(), { verbose: options.verbose });
+  if (isVisible) {
+    report('waiting for the selector to disappear ...', options.verbose);
+    await waitUntil(() => selector.isNotVisible(), `'${selector.toString()}' is visible.`, options);
+  }
+
+  const handle = await selector.getHandle();
+  return await isHandleNotVisible(handle, { verbose: options.verbose });
+}
