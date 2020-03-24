4af0791266e2ca93585ce126810346caa7b6edbd
hdorgeval <henri.dorgeval@outlook.fr>
2020-03-24 17:30:14 +0100

    feat(selector-actions): be able to check if selector has a class

--- src/actions/handle-actions/index.ts
+++ src/actions/handle-actions/index.ts
@@ -8,6 +8,7 @@ export * from './get-inner-text-of-handle';
 export * from './get-nth-handle';
 export * from './get-parent-of-handles';
 export * from './get-value-of-handle';
+export * from './has-handle-class';
 export * from './has-handle-exact-value';
 export * from './has-handle-focus';
 export * from './has-handle-text';
---
+++ src/actions/selector-actions/has-selector-object-class/has-selector-object-class.ts
@@ -0,0 +1,27 @@
+import { waitUntil, report, WaitUntilOptions } from '../../../utils';
+import { SelectorFluent } from '../../../selector-api';
+import { hasHandleClass } from '../../handle-actions';
+import { Page } from 'playwright';
+
+export async function hasSelectorObjectClass(
+  selector: SelectorFluent,
+  expectedClass: string,
+  page: Page | undefined,
+  options: WaitUntilOptions,
+): Promise<boolean> {
+  if (!page) {
+    throw new Error(
+      `Cannot check that '${selector.toString()}' has class '${expectedClass}' because no browser has been launched`,
+    );
+  }
+
+  report('waiting for the selector to appear in DOM ...', options.verbose);
+  await waitUntil(
+    () => selector.exists(),
+    `Cannot check that '${selector.toString()}' has class '${expectedClass}' because this selector was not found in DOM`,
+    options,
+  );
+
+  const handle = await selector.getHandle();
+  return await hasHandleClass(handle, expectedClass);
+}
