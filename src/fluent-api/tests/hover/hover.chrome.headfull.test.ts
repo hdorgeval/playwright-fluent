f280f2df636c17434c1bc71ebc7df1cd63b339c1
hdorgeval <henri.dorgeval@outlook.fr>
2020-03-24 09:39:31 +0100

    test(fluent-api): add test on chrome

---
+++ src/fluent-api/tests/hover/hover.chrome.headfull.test.ts
@@ -0,0 +1,70 @@
+import * as SUT from '../../playwright-fluent';
+import * as path from 'path';
+describe('Playwright Fluent - hover', (): void => {
+  let p: SUT.PlaywrightFluent;
+  beforeEach((): void => {
+    jest.setTimeout(60000);
+    p = new SUT.PlaywrightFluent();
+  });
+  afterEach(
+    async (): Promise<void> => {
+      await p.close();
+    },
+  );
+  test('should wait until selector exists - chrome', async (): Promise<void> => {
+    // Given
+    const url = `file:${path.join(__dirname, 'hover.test.html')}`;
+    const selector = '#dynamically-added-input';
+
+    // When
+    await p
+      .withBrowser('chrome')
+      .withOptions({ headless: false })
+      .withCursor()
+      .navigateTo(url)
+      .hover(selector);
+
+    // Then
+    const value = await p.getValueOf(selector);
+    expect(value).toBe('I am hovered');
+  });
+
+  test('should wait until selector object exists - chrome', async (): Promise<void> => {
+    // Given
+    const url = `file:${path.join(__dirname, 'hover.test.html')}`;
+    const selector = p.selector('input').withValue('dynamically added');
+
+    // When
+    await p
+      .withBrowser('chrome')
+      .withOptions({ headless: false })
+      .withCursor()
+      .navigateTo(url)
+      .hover(selector);
+
+    // Then
+    const finalValue = await p.getValueOf('#dynamically-added-input');
+    expect(finalValue).toBe('I am hovered');
+  });
+
+  test('should hover - chrome', async (): Promise<void> => {
+    // Given
+    const url = 'https://reactstrap.github.io/components/form';
+
+    // When
+    await p
+      .withBrowser('chrome')
+      .withOptions({ headless: false })
+      .withCursor()
+      .emulateDevice('iPhone 6 landscape')
+      .navigateTo(url)
+      .hover('#exampleEmail')
+      .hover('#examplePassword')
+      .hover('#exampleSelect')
+      .hover('#exampleSelectMulti')
+      .hover('"Submit"');
+
+    // Then
+    expect(true).toBe(true);
+  });
+});
