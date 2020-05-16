2ff38ae301fc1744fb50599261fcb29fa3b68a22
hdorgeval <henri.dorgeval@outlook.fr>
2020-05-16 19:46:44 +0200

    test(fluent-api): add test on withWindowSize()

---
+++ src/fluent-api/tests/with-windowsize/with-windowsize.webkit.headfull.test.ts
@@ -0,0 +1,41 @@
+import { PlaywrightFluent } from '../../playwright-fluent';
+import { LaunchOptions } from '../../../actions';
+import { windowsize } from '../../../devices';
+
+describe.skip('Playwright Fluent - withWindowSize', (): void => {
+  let p: PlaywrightFluent;
+  beforeEach((): void => {
+    jest.setTimeout(60000);
+    p = new PlaywrightFluent();
+  });
+  afterEach(
+    async (): Promise<void> => {
+      await p.close();
+    },
+  );
+
+  test('should target webkit in headfull mode with window size 800x600', async (): Promise<
+    void
+  > => {
+    // Given
+    const browser = 'webkit';
+    const options: LaunchOptions = {
+      headless: false,
+    };
+    const url = 'https://reactstrap.github.io/components/form';
+    const size = windowsize._800x600;
+
+    // When
+    // prettier-ignore
+    await p
+      .withBrowser(browser)
+      .withOptions(options)
+      .withWindowSize(size)
+      .navigateTo(url);
+
+    // Then
+    const windowState = await p.getCurrentWindowState();
+    expect(Math.abs(windowState.outerWidth - size.width)).toBeLessThanOrEqual(10);
+    expect(Math.abs(windowState.outerHeight - size.height)).toBeLessThanOrEqual(10);
+  });
+});
