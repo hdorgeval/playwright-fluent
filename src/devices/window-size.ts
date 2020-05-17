59e79d684c84870d8cebb827f709645023a51fd7
hdorgeval <henri.dorgeval@outlook.fr>
2020-05-17 16:28:31 +0200

    feat(devices): add screen resolutions

--- src/devices/window-size.ts
+++ src/devices/window-size.ts
@@ -67,9 +67,51 @@ export const _1600x900: WindowSize = {
   height: 900,
 };
 
+export const _1600x1024: WindowSize = {
+  width: 1600,
+  height: 1024,
+};
+
+export const _1600x1200: WindowSize = {
+  width: 1600,
+  height: 1200,
+};
+
+export const _1920x1080: WindowSize = {
+  width: 1920,
+  height: 1080,
+};
+
+export const _1920x1200: WindowSize = {
+  width: 1920,
+  height: 1200,
+};
+
+export const _1920x1440: WindowSize = {
+  width: 1920,
+  height: 1440,
+};
+
+export const _2560x1440: WindowSize = {
+  width: 2560,
+  height: 1440,
+};
+
+export const _3840x2160: WindowSize = {
+  width: 3840,
+  height: 2160,
+};
+
 export const sizeOf = {
   _800x600,
   _1024x768,
   _1280x720,
+  _1600x1024,
+  _1600x1200,
   _1600x900,
+  _1920x1080,
+  _1920x1200,
+  _1920x1440,
+  _2560x1440,
+  _3840x2160,
 };
