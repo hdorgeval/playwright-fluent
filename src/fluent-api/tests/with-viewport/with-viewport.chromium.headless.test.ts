37ff32e1ce5b853b7c804b322a6687bb5b7f1f55
hdorgeval <henri.dorgeval@outlook.fr>
2020-05-17 16:29:18 +0200

    test(fluent-api): add test

---
+++ src/fluent-api/tests/with-viewport/with-viewport.chromium.headless.test.ts
@@ -0,0 +1,93 @@
+import { PlaywrightFluent } from '../../playwright-fluent';
+import { LaunchOptions } from '../../../actions';
+import { sizeOf, Viewport } from '../../../devices';
+describe('Playwright Fluent - withViewPort', (): void => {
+  let p: PlaywrightFluent;
+  beforeEach((): void => {
+    jest.setTimeout(30000);
+    p = new PlaywrightFluent();
+  });
+  afterEach(
+    async (): Promise<void> => {
+      await p.close();
+    },
+  );
+  test('should target chromium in headless with viewport size 800x600', async (): Promise<void> => {
+    // Given
+    const browser = 'chromium';
+    const options: LaunchOptions = {
+      headless: true,
+    };
+    const url = 'https://reactstrap.github.io/components/form';
+    const viewport: Viewport = {
+      ...sizeOf._800x600,
+    };
+
+    // When
+    // prettier-ignore
+    await p
+      .withBrowser(browser)
+      .withOptions(options)
+      .withViewport(viewport)
+      .navigateTo(url);
+
+    // Then
+    const windowState = await p.getCurrentWindowState();
+    expect(Math.abs(windowState.innerWidth - viewport.width)).toBeLessThanOrEqual(10);
+    expect(Math.abs(windowState.innerHeight - viewport.height)).toBeLessThanOrEqual(10);
+  });
+
+  test('should target chromium in headless with viewport size 1280x720', async (): Promise<
+    void
+  > => {
+    // Given
+    const browser = 'chromium';
+    const options: LaunchOptions = {
+      headless: true,
+    };
+    const url = 'https://reactstrap.github.io/components/form';
+    const viewport: Viewport = {
+      ...sizeOf._1280x720,
+    };
+
+    // When
+    // prettier-ignore
+    await p
+      .withBrowser(browser)
+      .withOptions(options)
+      .withViewport(viewport)
+      .navigateTo(url);
+
+    // Then
+    const windowState = await p.getCurrentWindowState();
+    expect(Math.abs(windowState.innerWidth - viewport.width)).toBeLessThanOrEqual(10);
+    expect(Math.abs(windowState.innerHeight - viewport.height)).toBeLessThanOrEqual(10);
+  });
+
+  test('should target chromium in headless with viewport size 1600x900', async (): Promise<
+    void
+  > => {
+    // Given
+    const browser = 'chromium';
+    const options: LaunchOptions = {
+      headless: true,
+    };
+    const url = 'https://reactstrap.github.io/components/form';
+    const viewport: Viewport = {
+      ...sizeOf._1600x900,
+    };
+
+    // When
+    // prettier-ignore
+    await p
+      .withBrowser(browser)
+      .withOptions(options)
+      .withViewport(viewport)
+      .navigateTo(url);
+
+    // Then
+    const windowState = await p.getCurrentWindowState();
+    expect(Math.abs(windowState.innerWidth - viewport.width)).toBeLessThanOrEqual(10);
+    expect(Math.abs(windowState.innerHeight - viewport.height)).toBeLessThanOrEqual(10);
+  });
+});
