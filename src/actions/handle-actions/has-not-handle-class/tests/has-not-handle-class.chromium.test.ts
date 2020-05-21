9ca490453fd9e48a3d38119c753f486aa1074a6d
hdorgeval <henri.dorgeval@outlook.fr>
2020-05-21 13:47:41 +0200

    feat(handle-actions): be able to check that a selector does not have a class

---
+++ src/actions/handle-actions/has-not-handle-class/tests/has-not-handle-class.chromium.test.ts
@@ -0,0 +1,59 @@
+import * as SUT from '../index';
+import { Browser, chromium } from 'playwright';
+import * as path from 'path';
+
+describe('handle does not have class', (): void => {
+  let browser: Browser | undefined = undefined;
+  beforeEach((): void => {
+    jest.setTimeout(60000);
+  });
+  afterEach(
+    async (): Promise<void> => {
+      if (browser) {
+        await browser.close();
+      }
+    },
+  );
+
+  test('should return true when selector does not have class', async (): Promise<void> => {
+    // Given
+    browser = await chromium.launch({ headless: true });
+    const browserContext = await browser.newContext({ viewport: null });
+    const page = await browserContext.newPage();
+    const url = `file:${path.join(__dirname, 'has-not-handle-class.test.html')}`;
+    await page.goto(url);
+
+    const selector = '#with-two-classes';
+    const handle = await page.$(selector);
+
+    // When
+    const result1 = await SUT.hasNotHandleClass(handle, 'foobar');
+    const result2 = await SUT.hasNotHandleClass(handle, 'barfoo');
+
+    // Then
+    expect(handle).toBeDefined();
+    expect(result1).toBe(true);
+    expect(result2).toBe(true);
+  });
+
+  test('should return false when selector has the class', async (): Promise<void> => {
+    // Given
+    browser = await chromium.launch({ headless: true });
+    const browserContext = await browser.newContext({ viewport: null });
+    const page = await browserContext.newPage();
+    const url = `file:${path.join(__dirname, 'has-not-handle-class.test.html')}`;
+    await page.goto(url);
+
+    const selector = '#with-two-classes';
+    const handle = await page.$(selector);
+
+    // When
+    const result1 = await SUT.hasNotHandleClass(handle, 'foo');
+    const result2 = await SUT.hasNotHandleClass(handle, 'bar');
+
+    // Then
+    expect(handle).toBeDefined();
+    expect(result1).toBe(false);
+    expect(result2).toBe(false);
+  });
+});
