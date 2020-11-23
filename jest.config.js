c0a1a1d7e53336a0d514c4d20177f945fdbdab68
hdorgeval <henri.dorgeval@outlook.fr>
2020-11-23 12:28:26 +0100

    chore(jest): update configuration

--- jest.config.js
+++ jest.config.js
@@ -78,7 +78,7 @@ module.exports = {
   // A set of global variables that need to be available in all test environments
   globals: {
     'ts-jest': {
-      tsConfig: 'tsconfig.json',
+      tsconfig: 'tsconfig.json',
     },
   },
 
