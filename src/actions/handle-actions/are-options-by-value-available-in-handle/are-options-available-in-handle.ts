84391a2e7865f2cb9c9bb15f1fbed49da1ed10db
hdorgeval <henri.dorgeval@outlook.fr>
2021-01-08 13:47:12 +0100

    feat(handle-actions): be able to check if options exist in a select

---
+++ src/actions/handle-actions/are-options-by-value-available-in-handle/are-options-available-in-handle.ts
@@ -0,0 +1,22 @@
+import { isOptionByValueAvailableInHandle } from '../is-option-by-value-available-in-handle';
+import { ElementHandle } from 'playwright';
+
+export async function areOptionsByValueAvailableInHandle(
+  selector: ElementHandle<Element> | null | undefined,
+  name: string,
+  expectedOptionValues: string[],
+): Promise<boolean> {
+  if (expectedOptionValues.length === 0) {
+    throw new Error('No option to check: you must specify at least one option');
+  }
+
+  for (let index = 0; index < expectedOptionValues.length; index++) {
+    const expectedOptionValue = expectedOptionValues[index];
+    const isAvailable = await isOptionByValueAvailableInHandle(selector, name, expectedOptionValue);
+    if (!isAvailable) {
+      return false;
+    }
+  }
+
+  return true;
+}
