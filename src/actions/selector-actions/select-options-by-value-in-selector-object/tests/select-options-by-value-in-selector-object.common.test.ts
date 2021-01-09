23d0c410561eeb7ed96c34b6ae4f37a0acee70e5
hdorgeval <henri.dorgeval@outlook.fr>
2021-01-09 17:37:27 +0100

    feat(selector-actions): be able to select options by value in selector

---
+++ src/actions/selector-actions/select-options-by-value-in-selector-object/tests/select-options-by-value-in-selector-object.common.test.ts
@@ -0,0 +1,38 @@
+import { PlaywrightFluent } from '../../../../fluent-api';
+import * as SUT from '../index';
+import { defaultSelectOptions } from '../../../handle-actions';
+describe('select options by value in selector object', (): void => {
+  let p: PlaywrightFluent;
+  beforeEach((): void => {
+    jest.setTimeout(30000);
+    p = new PlaywrightFluent();
+  });
+  afterEach(
+    async (): Promise<void> => {
+      await p.close();
+    },
+  );
+
+  test('should throw an error when browser has not been launched', async (): Promise<void> => {
+    // Given
+
+    // When
+    let result: Error | undefined = undefined;
+    try {
+      const selector = p.selector('foobar');
+      await SUT.selectOptionsByValueInSelectorObject(
+        selector,
+        ['value 1'],
+        p.currentPage(),
+        defaultSelectOptions,
+      );
+    } catch (error) {
+      result = error;
+    }
+
+    // Then
+    const expectedErrorMessage =
+      "Cannot select options 'value 1' in 'selector(foobar)' because no browser has been launched";
+    expect(result && result.message).toContain(expectedErrorMessage);
+  });
+});
