5d5b6774a02a35bddc343c3bea2acfcab405e059
hdorgeval <henri.dorgeval@outlook.fr>
2021-04-15 22:21:32 +0200

    docs(fluent-api): update doc of getToday()

--- src/fluent-api/playwright-fluent.ts
+++ src/fluent-api/playwright-fluent.ts
@@ -1503,6 +1503,7 @@ export class PlaywrightFluent implements PromiseLike<void> {
    *    .withBrowser('chromium')
    *    .withOptions({ headless: true })
    *    .withCursor()
+   *    .withTimezone('Asia/Tokyo')
    *    .navigateTo(url);
    *
    *  const today = await p.getToday('yyyy-mm-dd');
