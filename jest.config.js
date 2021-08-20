d819d6e7b88d27ca274c33464bda2adc9539e9c3
hdorgeval <henri.dorgeval@outlook.fr>
2021-08-20 18:35:29 +0200

    chore(jest): update timeout value

--- jest.config.js
+++ jest.config.js
@@ -182,7 +182,7 @@ module.exports = {
 
   // Setting this value to "fake" allows the use of fake timers for functions such as "setTimeout"
   // timers: "real",
-  testTimeout: 60000,
+  testTimeout: 180000,
 
   // A map from regular expressions to paths to transformers
   transform: {
