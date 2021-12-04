4ceb00ce2f588fe61a0bc91b80f17f4d1a4f59c7
hdorgeval <henri.dorgeval@outlook.fr>
2021-12-04 21:35:31 +0100

    test(fluent-api): add test to the tracing API

--- src/fluent-api/tests/with-tracing/with-tracing.chromium.headfull.test.ts
+++ src/fluent-api/tests/with-tracing/with-tracing.chromium.headfull.test.ts
@@ -13,7 +13,7 @@ describe('Playwright Fluent - withTracing()', (): void => {
     // Given
     const browser = 'chromium';
     const url = 'https://reactstrap.github.io';
-    const tracePath = path.join(__dirname, 'trace1.zip');
+    const tracePath = path.join(__dirname, 'trace1.chromium.zip');
     expect(fileExists(tracePath)).toBe(false);
 
     // When
@@ -31,5 +31,52 @@ describe('Playwright Fluent - withTracing()', (): void => {
 
     // Then
     expect(fileExists(tracePath)).toBe(true);
+  });
+
+  test('should handle multiple traces with chromium in headfull mode', async (): Promise<void> => {
+    // Given
+    const browser = 'chromium';
+    const url = 'https://reactstrap.github.io';
+    const firstTracePath = path.join(__dirname, 'trace01.chromium.zip');
+    expect(fileExists(firstTracePath)).toBe(false);
+
+    const secondTracePath = path.join(__dirname, 'trace02.chromium.zip');
+    expect(fileExists(secondTracePath)).toBe(false);
+
+    const componentsTree = 'div#components';
+
+    const formsComponents = p
+      .selector('div#storybook-explorer-tree')
+      .find('button')
+      .withText('Forms');
+    const storyBookIframe = 'iframe#storybook-preview-iframe';
+    const checkMeOut = p.selector('label').withText('Check me out').parent().find('input');
+
+    // When
+    await p
+      .withBrowser(browser)
+      .withOptions({ headless: false })
+      .withTracing()
+      .withCursor()
+      .startTracing({ title: 'my first trace' })
+      .navigateTo(url)
+      .stopTracingAndSaveTrace({ path: firstTracePath })
+      .startTracing({ title: 'my second trace' })
+      .hover(componentsTree)
+      .hover(formsComponents)
+      .click(formsComponents)
+      .switchToIframe(storyBookIframe)
+      .check(checkMeOut)
+      .expectThatSelector(checkMeOut)
+      .isChecked()
+      .stopTracingAndSaveTrace({ path: secondTracePath })
+      .waitUntil(async () => fileExists(secondTracePath), {
+        throwOnTimeout: false,
+        wrapPredicateExecutionInsideTryCatch: true,
+      });
+
+    // Then
+    expect(fileExists(firstTracePath)).toBe(true);
+    expect(fileExists(secondTracePath)).toBe(true);
   });
 });
