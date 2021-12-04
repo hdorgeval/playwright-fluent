d057e466e0f229c5063d86e3425053aa5852039e
hdorgeval <henri.dorgeval@outlook.fr>
2021-12-04 22:09:58 +0100

    chore(lint): remove unused disable directive detected by eslint dependency update

--- src/utils/get-chrome-path/tests/get-chrome-path.test.ts
+++ src/utils/get-chrome-path/tests/get-chrome-path.test.ts
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-disable @typescript-eslint/ban-types */
 describe('get-chrome-path', (): void => {
   afterEach((): void => {
     jest.resetModules();
