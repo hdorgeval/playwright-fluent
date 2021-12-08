4fb2f69bd05f479fc1df2d0824f66ab6cf10c9e2
hdorgeval <henri.dorgeval@outlook.fr>
2021-12-08 22:44:19 +0100

    chore(jest): add intellisense in jest configuration file

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 // For a detailed explanation regarding each configuration property, visit:
 // https://jestjs.io/docs/en/configuration.html
+/** @type {import('@jest/types').Config.InitialOptions} */
 
 module.exports = {
   // All imported modules in your tests should be mocked automatically
