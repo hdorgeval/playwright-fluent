00474f117ef733ef4bc4a55698a8a6894744d39b
hdorgeval <henri.dorgeval@outlook.fr>
2021-12-09 10:35:16 +0100

    chore(ci): try reduce tests duration

--- jest.config.js
+++ jest.config.js
@@ -89,7 +89,7 @@ module.exports = {
     },
   },
 
-  maxConcurrency: 2,
+  maxConcurrency: 3,
 
   // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
   maxWorkers: '75%',
