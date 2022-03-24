f15e44a9ade4b8748eddbda974ac7567917542bb
hdorgeval <henri.dorgeval@outlook.fr>
2022-03-24 12:50:03 +0100

    test(fluent-api): increase waiting time

--- src/fluent-api/tests/on-request-to-respond-with/on-request-to-respond-with.chromium.post.test.ts
+++ src/fluent-api/tests/on-request-to-respond-with/on-request-to-respond-with.chromium.post.test.ts
@@ -82,7 +82,7 @@ describe('Playwright Fluent - onRequestTo(url).respondWith()', (): void => {
     // Then requests to /foobar should be intercepted
     // And response should be mocked
     await p.waitForStabilityOf(async () => p.getRecordedRequestsTo('/foobar').length, {
-      stabilityInMilliseconds: 1000,
+      stabilityInMilliseconds: 5000,
     });
     const foobarRequests = p.getRecordedRequestsTo('/foobar');
     expect(Array.isArray(foobarRequests)).toBe(true);
