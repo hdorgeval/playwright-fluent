51bd50a214d42d5c9f3c90fb441aec2cec76b5d2
hdorgeval <henri.dorgeval@outlook.fr>
2022-03-25 08:24:46 +0100

    test(dialogs): fix concurrency execution of tests

--- src/actions/page-actions/wait-for-dialog/tests/wait-for-dialog.chromium.test.ts
+++ src/actions/page-actions/wait-for-dialog/tests/wait-for-dialog.chromium.test.ts
@@ -1,22 +1,17 @@
 import * as SUT from '../index';
 import { recordPageDialogs } from '../../record-page-dialogs';
-import { Browser, chromium, Dialog } from 'playwright';
+import { chromium, Dialog } from 'playwright';
 import * as path from 'path';
 
 describe('wait for page dialog to open', (): void => {
-  let browser: Browser | undefined = undefined;
-
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   beforeEach((): void => {});
-  afterEach(async (): Promise<void> => {
-    if (browser) {
-      await browser.close();
-    }
-  });
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  afterEach(async (): Promise<void> => {});
 
   test('should wait for confirm dialog to open', async (): Promise<void> => {
     // Given
-    browser = await chromium.launch({ headless: true });
+    const browser = await chromium.launch({ headless: true });
     const context = await browser.newContext({ viewport: null });
     const page = await context.newPage();
     let dialogIsOpened = false;
@@ -36,11 +31,12 @@ describe('wait for page dialog to open', (): void => {
     expect(dialogIsOpened).toBe(true);
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     expect(openedDialog!.type()).toBe('confirm');
+    await browser.close();
   });
 
   test('should wait for alert dialog to open', async (): Promise<void> => {
     // Given
-    browser = await chromium.launch({ headless: true });
+    const browser = await chromium.launch({ headless: true });
     const context = await browser.newContext({ viewport: null });
     const page = await context.newPage();
     let dialogIsOpened = false;
@@ -60,11 +56,12 @@ describe('wait for page dialog to open', (): void => {
     expect(dialogIsOpened).toBe(true);
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     expect(openedDialog!.type()).toBe('alert');
+    await browser.close();
   });
 
   test('should wait for prompt dialog to open', async (): Promise<void> => {
     // Given
-    browser = await chromium.launch({ headless: true });
+    const browser = await chromium.launch({ headless: true });
     const context = await browser.newContext({ viewport: null });
     const page = await context.newPage();
     let dialogIsOpened = false;
@@ -84,5 +81,6 @@ describe('wait for page dialog to open', (): void => {
     expect(dialogIsOpened).toBe(true);
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     expect(openedDialog!.type()).toBe('prompt');
+    await browser.close();
   });
 });
